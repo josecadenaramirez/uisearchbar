3a5f53e9dafcccd453bf65ed3ae8b2e254b6ec96
José Cadena <cadena@tejuino.mx>
2018-08-07 19:53:14 -0500

    Initial Commit

---
+++ searchBar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  searchBar
+//
+//  Created by José Cadena on 07/08/18.
+//  Copyright © 2018 José Cadena. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
